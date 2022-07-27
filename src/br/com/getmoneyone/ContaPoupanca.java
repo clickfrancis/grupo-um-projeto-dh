19229c0d2507cf529d9985de7e439bbd924222fb
clickfrancis <francis.source@gmail.com>
2022-07-27 19:21:21 -0300

    feat: criado métodos da classe poupança.

--- src/br/com/getmoneyone/ContaPoupanca.java
+++ src/br/com/getmoneyone/ContaPoupanca.java
@@ -2,9 +2,12 @@ package br.com.getmoneyone;
 
 public class ContaPoupanca extends Conta {
 
-    protected ContaPoupanca(int numero, String cpf) {
+    protected ContaPoupanca(int numero, String cpf, int diaAniversarioPoupanca) {
         super(numero, cpf);
+        this.diaAniversarioPoupanca = diaAniversarioPoupanca;
     }
+
+    private final int diaAniversarioPoupanca;
 
     @Override
     protected void debito() {
@@ -16,4 +19,24 @@ public class ContaPoupanca extends Conta {
 
     }
 
+    public int getDiaAniversarioPoupanca() {
+        return diaAniversarioPoupanca;
+    }
+
+    public void correcao(int diaInfoCliente) {
+
+        if (getSaldo() > 0.0){
+            if(diaInfoCliente == diaAniversarioPoupanca) {
+                double saldo = (getSaldo()*0.05)+getSaldo();
+                setSaldo(saldo);
+                System.out.println("Seu saldo foi ajustado para : "+saldo);
+            }
+            else {
+                System.out.println("Ajuste no saldo não realizado.");
+                System.out.println("O dia informado não é o mesmo dia do aniversário da poupança.");
+            }
+        } else {
+            System.out.println("Saldo insuficiente para gerar rendimento.");
+        }
+    }
 }
