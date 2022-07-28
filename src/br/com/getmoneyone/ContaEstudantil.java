4aaade2404fd15e97b96a4e8349b1548d26924e8
liviabbotelho <liviabbotelho@hotmail.com>
2022-07-27 22:13:21 -0300

    Implementação da Classe ContaEstudantil

--- src/br/com/getmoneyone/ContaEstudantil.java
+++ src/br/com/getmoneyone/ContaEstudantil.java
@@ -1,5 +1,41 @@
 package br.com.getmoneyone;
 
-public class ContaEstudantil {
+public class ContaEstudantil extends Conta{
+    private double limiteEstudantil = 5000;
 
+    public double getLimiteEstudantil() {
+        return limiteEstudantil;
+    }
+
+    protected ContaEstudantil(int numero, String cpf) {
+
+        super(numero, cpf);
+        setSaldo(0.0);
+    }
+
+    @Override
+    protected void debito(double valor) {
+        if(valor <= getSaldo()){
+            setSaldo(getSaldo()-valor);
+        }
+        else{
+            System.out.println("Saldo insuficiente, utilize seu limite estudantil");
+        }
+    }
+
+    @Override
+    public void credito(double valor) {
+        setSaldo(getSaldo()+valor);
+    }
+
+    public void usarEstudantil(double valor){
+
+        if(valor <= limiteEstudantil) {
+            credito(valor);
+            limiteEstudantil -= valor;
+        }
+        else {
+            System.out.println("Limite estudantil insuficiente");
+        }
+    }
 }
