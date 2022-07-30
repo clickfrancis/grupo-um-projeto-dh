f45b51df8885ab741eb647c05cc0a262c4ecd709
Franci Araujo <franci_araujoroberto@hotmail.com>
2022-07-30 03:53:42 +0200

    feito método credito e debito

--- src/br/com/getmoneyone/ContaEmpresa.java
+++ src/br/com/getmoneyone/ContaEmpresa.java
@@ -1,4 +1,42 @@
 package br.com.getmoneyone;
 
-public class ContaEmpresa {
+public class ContaEmpresa extends Conta {
+	
+	private double emprestimoEmpresa;
+	
+
+	protected ContaEmpresa(int numero, String cpf) {
+		super(numero, cpf);
+	    setSaldo(0.0);
+	}
+
+	@Override
+	protected void debito(double saque) {
+		double saldo = getSaldo();
+		if(saldo >= saque) {
+			saldo = saldo - saque;
+			setSaldo(saldo);
+			System.out.println("DEBITO " + saque);
+		}else{
+		}
+	System.out.println("SALDO " + saldo);
+		
+	}
+
+	@Override
+	public void credito(double deposito) {
+		double saldo = getSaldo();
+		saldo = saldo + deposito;
+		setSaldo(saldo);
+		System.out.println("CREDITO " + deposito);
+		System.out.println("SALDO " + saldo);
+	}
+	
+	public void pedirEmprestimo() {
+		
+		
+	}
+	
+	
 }
+
