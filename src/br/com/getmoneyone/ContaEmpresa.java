9c9876937848bcfaa7d7e01f8aa1e4855b09577a
Franci Araujo <franci_araujoroberto@hotmail.com>
2022-07-31 03:29:13 +0200

    criado commit conta empresa metodo menu

--- src/br/com/getmoneyone/ContaEmpresa.java
+++ src/br/com/getmoneyone/ContaEmpresa.java
@@ -1,8 +1,10 @@
 package br.com.getmoneyone;
 
+import java.util.Scanner;
+
 public class ContaEmpresa extends Conta {
 	
-	private double emprestimoEmpresa;
+	private double emprestimoEmpresa = 10000;
 	
 
 	protected ContaEmpresa(int numero, String cpf) {
@@ -13,13 +15,10 @@ public class ContaEmpresa extends Conta {
 	@Override
 	protected void debito(double saque) {
 		double saldo = getSaldo();
-		if(saldo >= saque) {
 			saldo = saldo - saque;
 			setSaldo(saldo);
 			System.out.println("DEBITO " + saque);
-		}else{
-		}
-	System.out.println("SALDO " + saldo);
+	        System.out.println("SALDO " + saldo);
 		
 	}
 
@@ -32,11 +31,57 @@ public class ContaEmpresa extends Conta {
 		System.out.println("SALDO " + saldo);
 	}
 	
-	public void pedirEmprestimo() {
-		
-		
+	public double getEmprestimoEmpresa() {
+		return emprestimoEmpresa;
 	}
 	
+	public void pedirEmprestimo(double saque) {
+		double saldo = getSaldo();
+		emprestimoEmpresa = emprestimoEmpresa - saque;
+	    saldo = saldo + saque;
+		setSaldo(saldo);
+		System.out.println("SALDO + VALOR DE EMPRESTIMO SOLICITADO" + saldo); 
+		System.out.println("");
+	}
 	
+	public void menu() {
+		char tipoMovimentaçao;
+		char continuar;
+		
+		Scanner entrada = new Scanner (System.in);
+		
+		for (int i = 0; i < 10; i ++) {
+			System.out.println("Valor de emprestimo: " + emprestimoEmpresa);
+			System.out.println("SALDO ATUAL " + getSaldo()); 
+			System.out.println("MOVIMENTO D-Debito ou C-Credito ou E-Emprestimo");
+			tipoMovimentaçao = entrada.next().charAt(0);
+			
+			if(tipoMovimentaçao == 'C') {
+				System.out.println("Valor do credito: "); 
+				double valorDeposito = entrada.nextDouble();
+				credito(valorDeposito);	
+			}else if(tipoMovimentaçao == 'D') {
+				System.out.println("Valor debito: ");
+				double valorSaque = entrada.nextDouble();
+				debito(valorSaque);
+			}else if(tipoMovimentaçao == 'E'){
+				double valorEmprestimo = entrada.nextDouble();
+				pedirEmprestimo(valorEmprestimo);
+			System.out.println("Opçao invalida");
+			}
+			
+			System.out.println("Continuar S-Sim ou N-Nao");
+			continuar = entrada.next().charAt(0);
+			if(continuar == 'S' && i >10) {
+				System.out.println("Voce chegou no fim da sua movimentaçao");
+			}else if (continuar == 'N') {
+				System.out.println("Fim da operaçao");
+				break;
+			}
+		}
+		System.out.println("Seu saldo atualizado seria " + getSaldo() + "Seu valor atual de emprestimo seria de  " + emprestimoEmpresa);
+			
+		
+	}
 }
 
